f76fefa0e991f161ff520ad9a1db86f2a112d02a
Tatsubooom <tatubon0920@gmail.com>
2026-05-20 20:15:36 +0900

    front UI change

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
+func novelReadfile() (string, error) {
+	data, err := os.ReadFile("hana.txt")
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func novelget(c *gin.Context) {
+	text, err := novelReadfile()
+	if err != nil {
+		c.JSON(500, gin.H{"error": "小説データの読み込みに失敗しました: " + err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"message": text})
+}
+
+func explpost(c *gin.Context) {
+
+}
+
 func main() {
-	http.HandleFunc("/api/test", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `{"message": "Hello from Go!"}`)
-	})
-	fmt.Println("Server is running on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	r := gin.Default()
+	// CORS設定
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:3000"},
+	}))
+
+	//小説本文呼びだし
+	r.GET("/api/novel", novelget)
+	//AIコール
+	r.POST("api/explanation", explpost)
+	r.Run(":8080")
 }
